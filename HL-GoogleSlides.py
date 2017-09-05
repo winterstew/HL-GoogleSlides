01dde348dbf22953b7f16ddfb02607c6d80c9355
WinterStew <github@winterstew.com>
2017-09-05 06:11:07 -0500

    Revert "added spellclasses"
    This reverts commit d24a32126d7f97262a9e43a4d007c694ac94d5f0.

--- HL-GoogleSlides.py
+++ HL-GoogleSlides.py
@@ -132,7 +132,6 @@ REPLACEFLAGS = {
 'spells known..': "get_sortedspells(character,'spellsknown')",
 'spells memorized..': "get_sortedspells(character,'spellsmemorized')",
 'spells book..': "get_sortedspells(character,'spellbook')",
-'spellclasses..': "get_spellclasses(character.find('spellclasses'))",
 'npc description': "get_textformatch(character.find('npc'),('description',''),('',''))", 
 'npc basics': "get_textformatch(character.find('npc'),('description',''),('',''))",
 'npc basics-goals': "get_textformatch(character.find('npc'),('basics','npcinfo'),('name','Motivations & Goals'))",
@@ -191,18 +190,6 @@ def get_textformatch(npc,brchs,test):
                 if ni.get(test[0]) == test[1]: return ni.text
         else: return b1.text
     return ''
-
-def get_spellclasses(spellClasses):
-    rtn = ''
-    for sc in spellClasses.iter('spellclass'):
-        hasLevels = False
-        rt = '%s:' % (sc.get('name'))
-        for s in sc.iter('spelllevel'):
-            hasLevels = True
-            rt = '%s%s/%s,' % (rt,s.get('level'),s.get('maxcasts'))
-        if hasLevels:
-            rtn = '%s%s;' % (rtn,rt[0:-1])
-    return rtn
             
 def get_sortedspells(character,spName):
     spells = []
